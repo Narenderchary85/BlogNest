b63bcb57a4ddba64e81f14611d72526556f21b5d
Narenderchary85 <narenderchary85@gmail.com>
2025-09-18 08:56:15 +0530

    token

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -87,7 +87,6 @@ const AddBlog = ({ isOpen, onClose , fetchBlogs}) => {
 
       const response = await axios.post(`${import.meta.env.VITE_API_URL}/blog/addblog`, submitData, {
         headers: {
-            Authorization: `Bearer ${token}`,
           'Content-Type': 'multipart/form-data'
         },
         withCredentials:true
